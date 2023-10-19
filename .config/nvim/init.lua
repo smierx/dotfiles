a50cd49dd9df39dc43c9629634ade68091e96b71
Michel Dudas <mdudas@AirvonMichel2.fritz.box>
2023-10-19 10:50:05 +0200

    added new kyemaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,12 @@
 require("smierx.core")
 require("smierx.lazy")
 
+require("luasnip").config.set_config({ -- Setting LuaSnip config
 
+	-- Enable autotriggered snippets
+	enable_autosnippets = true,
 
+	-- Use Tab (or some other key if you prefer) to trigger visual selection
+	store_selection_keys = "<Tab>",
+})
+require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/LuaSnip/" })
---
+++ .config/nvim/lua/smierx/plugins/vimtex.lua
@@ -0,0 +1,11 @@
+return {
+	"lervag/vimtex",
+	init = function()
+		vim.g.vimtex_view_method = "skim"
+		-- vim.g.vimtex_view_general_viewer = "zathura"
+		-- vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
+		vim.g.vimtex_quickfix_enabled = 1
+		vim.g.vimtex_syntax_enabled = 1
+		vim.g.vimtex_quickfix_mode = 0
+	end,
+}
