7440a6cb47732461a5e90319c001c052802b155b
Michel Dudas <mdudas@AirvonMichel2.fritz.box>
2023-10-23 06:12:18 +0200

    fixed python path

---
+++ .config/nvim/LuaSnip/all.lua
@@ -0,0 +1,20 @@
+local ls = require("luasnip")
+-- some shorthands...
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+
+local date = function()
+	return { os.date("%Y-%m-%d") }
+end
+
+return {
+	s("hi", { t("Hello World!") }),
+}
