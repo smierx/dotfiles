c91b9fd8f12cb4c6e530e82b5588801dde337538
brhn3u <michel.dudas@googlemail.com>
2023-10-25 07:25:12 +0200

    changed theme

--- .config/nvim/lua/smierx/plugins/colorscheme.lua
+++ .config/nvim/lua/smierx/plugins/colorscheme.lua
@@ -1,10 +1,19 @@
 return {
   {
-    "bluz71/vim-nightfly-guicolors",
+    "folke/tokyonight.nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
+    layz = false,
+    opts = {
+      style = "storm",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent"
+      },
+    },
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme nightfly]])
+      vim.cmd([[colorscheme tokyonight]])
     end,
   },
 }
