cc4202db6b1753979251279dc5d4b6a193378d11
Michel Dudas <mdudas@AirvonMichel2.fritz.box>
2023-10-25 06:45:08 +0200

    added luasnip again

--- .config/nvim/lua/smierx/core/keymaps.lua
+++ .config/nvim/lua/smierx/core/keymaps.lua
@@ -52,29 +52,29 @@ keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available 
 keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
 
 -- luasnip
---local ls = require("luasnip")
---keymap.set("i", "<C-K>", function()
---	ls.expand()
---end, { silent = true })
---keymap.set("i", "<C-L>", function()
---	ls.jump(1)
---end, { silent = true })
---keymap.set("i", "<C-J>", function()
---	ls.jump(-1)
---end, { silent = true })
+local ls = require("luasnip")
+keymap.set("i", "<C-K>", function()
+	ls.expand()
+end, { silent = true })
+keymap.set("i", "<C-L>", function()
+	ls.jump(1)
+end, { silent = true })
+keymap.set("i", "<C-J>", function()
+	ls.jump(-1)
+end, { silent = true })
 
---keymap.set({ "i", "s" }, "<C-E>", function()
---	if ls.choice_active() then
---		ls.change_choice(1)
---	end
---end, { silent = true })
+keymap.set({ "i", "s" }, "<C-E>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end, { silent = true })
 
---vim.cmd([[
---" Use Tab to expand and jump through snippets
--- imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
---smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+vim.cmd([[
+" Use Tab to expand and jump through snippets
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
 
---" Use Shift-Tab to jump backwards through snippets
---imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
---smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
---]])
+" Use Shift-Tab to jump backwards through snippets
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]])
