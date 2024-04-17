9ad3c7ca6077fdfc67d16fda4e060a9c2d9b5466
smierx <michel.dudas@googlemail.com>
2024-04-17 17:45:55 +0200

    neovim ausgemistet

--- .config/nvim/lua/smierx/plugins/lsp/mason.lua
+++ .config/nvim/lua/smierx/plugins/lsp/mason.lua
@@ -5,15 +5,9 @@ return {
 		"jayp0521/mason-null-ls.nvim",
 	},
 	config = function()
-		-- import mason
 		local mason = require("mason")
-
-		-- import mason-lspconfig
 		local mason_lspconfig = require("mason-lspconfig")
-
-		-- import mason-null-ls
 		local mason_null_ls = require("mason-null-ls")
-
 		-- enable mason and configure icons
 		mason.setup({
 			ui = {
@@ -28,15 +22,6 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
-				"tsserver",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"svelte",
-				"lua_ls",
-				"graphql",
-				"emmet_ls",
-				"prismals",
 				"pyright",
 			},
 			-- auto-install configured servers (with lspconfig)
@@ -46,9 +31,7 @@ return {
 		mason_null_ls.setup({
 			-- list of formatters & linters for mason to install
 			ensure_installed = {
-				"prettier", -- ts/js formatter
 				"stylua", -- lua formatter
-				"eslint_d", -- ts/js linter
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true,
