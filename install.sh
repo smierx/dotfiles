3bff75543d8f930356901c1f67bdc1fdda88520a
smierx <michel.dudas*+|~>
2024-07-21 18:16:43 +0200

    updated install sh

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@ sudo apt update
 sudo apt install \
     build-essential \
     curl \
-    nodejs \
-    npm \
     fzf \
     ripgrep \
     pipx \
@@ -23,7 +21,20 @@ sudo apt install \
     wget \
     xz-utils \
     zlib1g-dev \
+    lua5.2 \
+    liblua5.2 \
+    unzip \
+    lua5.1 \
+    liblua5.1 \
+    ca-certificates \
+    gnupg \
 
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt update
+sudo apt install nodejs
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
@@ -43,3 +54,8 @@ pipx install pyright
 pipx install ruff-lsp
 
 
+wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz
+tar zxpf luarocks-3.11.1.tar.gz
+cd luarocks-3.11.1
+./configure && make && sudo make install
+sudo luarocks install luasocket
