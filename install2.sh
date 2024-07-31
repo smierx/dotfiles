5d6b6049cbe0353883f62d318b7836eb6d12f4eb
Smierx <39676747+smierx@users.noreply.github.com>
2024-07-31 16:40:01 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,1 +1,9 @@
-
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+echo ".cfg" >> .gitignore
+git clone --bare git@github.com:smierx/dotfiles.git $HOME/.cfg
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+rm .bashrc
+rm install.sh
+rm install2.sh
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
