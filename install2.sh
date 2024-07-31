05bc39426545253ecd4c757c0effadb542de8603
Smierx <39676747+smierx@users.noreply.github.com>
2024-07-31 20:55:09 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -8,3 +8,6 @@ rm install2.sh
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 source .bashrc
+rm -rf luarocks-3.11.1
+rm luarocks-3.11.1-tar.gz
+rm nvim-linux64.tar.gz
