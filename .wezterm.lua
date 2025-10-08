e940a3be6cd6fbb0ef283c36981bf5f2ecd21c64
smierx <michel.dudas@googlemail.com>
2025-10-08 13:20:11 +0200

    updated wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -16,65 +16,16 @@ elseif os:find("linux") then
 	config.default_prog = { "/usr/bin/bash", "-l" }
 end
 
-config.default_cwd = "~/projects/" --FUNKTIONIERT NICHT
 config.font = wezterm.font("GohuFont uni14 Nerd Font Mono", { weight = "Regular" })
 
 -- Set colorscheme
---config.color_scheme = "Horizon Dark (Gogh)"
+config.color_scheme = 'Tokyo Night'
 
-config.colors = {
-	foreground = "#C0C0C0",
-	background = "#121212",
-
-	cursor_bg = "#00FF5F", -- kräftiges Grün
-	cursor_border = "#00FF5F",
-	cursor_fg = "#002B1E",
-
-	selection_bg = "#333333",
-	selection_fg = "#FFFFFF",
-
-	ansi = {
-		"#1A1A1A",
-		"#2A2A2A",
-		"#3A3A3A",
-		"#4A4A4A",
-		"#5A5A5A",
-		"#6A6A6A",
-		"#7A7A7A",
-		"#8A8A8A",
-	},
-	brights = {
-		"#2A2A2A",
-		"#3A3A3A",
-		"#4A4A4A",
-		"#5A5A5A",
-		"#6A6A6A",
-		"#7A7A7A",
-		"#8A8A8A",
-		"#FFFFFF",
-	},
-
-	tab_bar = {
-		background = "#121212",
-		active_tab = {
-			bg_color = "#1A1A1A",
-			fg_color = "#FFFFFF",
-		},
-		inactive_tab = {
-			bg_color = "#002B1E",
-			fg_color = "#777777",
-		},
-		new_tab = {
-			bg_color = "#002B1E",
-			fg_color = "#444444",
-		},
-	},
-}
 -- Font settings
 config.font_size = 16
 
 -- Window setting/ appearance
-config.window_decorations = "RESIZE"
+--config.window_decorations = "RESIZE"
 config.enable_tab_bar = true
 -- config.window_background_opacity = 0.85
 -- config.macos_window_background_blur = 10
@@ -111,9 +62,9 @@ config.keys = {
 	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
 }
 -- Tab bar
--- config.enable_tab_bar = false
+config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
-config.tab_bar_at_bottom = true
+config.tab_bar_at_bottom = false
 config.status_update_interval = 1000
 
 -- Return the configuration to wezterm
